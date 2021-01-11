17aa38147b655e858867ad3b9ba99b7e5dde61ce
as30098 <alexspencer421@gmail.com>
2021-01-10 20:13:18 -0500

    commit tree 3ef26d9cf6e1e67355a2583a0065a7b651d99b9c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const arrayOfNames = ["Alex", "Mai", "Zach"];
+const eventName = "surprise";
+
+function writeCards(arrayOfNames, event) {
+  let newArr = [];
+  for (let i = 0; i < arrayOfNames.length; i++) {
+    newArr.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return newArr;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
